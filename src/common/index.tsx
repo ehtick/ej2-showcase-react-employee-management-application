956c7e0dbf87b2322d3048921801fbd135cc079b
farook <mohamedj@syncfusion.com>
2024-12-09 18:57:12 +0530

    sample added

---
+++ src/common/index.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { createRoot } from 'react-dom/client';
+import MyProfile from '../components/MyProfile';
+import Policies from '../components/Policies';
+import Achievements from '../components/Achievements';
+import Announcement from '../components/Announcement';
+import Organization from '../components/Organization';
+import EmployeeInfo from '../components/EmployeeInfo';
+import { HashRouter, Routes, Route, NavLink } from "react-router-dom";
+import "../../styles/index.css";
+
+const root = createRoot(document.getElementById("content-area") as HTMLElement);
+root.render(
+  <>
+    
+    <div className="xyz-management">
+      <div className="xyz-management-header">
+        <div className="header-content">XYZ Software PVT LTD</div>
+      </div>
+      <HashRouter>
+        <div className="xyz-management-content" style={{ width: '100%' }}>
+          <div className="row">
+            <div className="col-md-2 sidebar">
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/">
+                    My Profile
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/organization">
+                    Organization
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/policies">
+                    Policies
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/achievements">
+                    Achievements
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/announcement">
+                    Announcement
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-10 main-content">
+              <Routes>
+                <Route path="/" element={<MyProfile />} />
+                <Route path="/organization" element={<Organization />} />
+                <Route path="/policies" element={<Policies />} />
+                <Route path="/achievements" element={<Achievements />} />
+                <Route path="/announcement" element={<Announcement />} />
+                <Route path="/employeeinfo" element={<EmployeeInfo />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </HashRouter>
+    </div>
+  </>
+);
